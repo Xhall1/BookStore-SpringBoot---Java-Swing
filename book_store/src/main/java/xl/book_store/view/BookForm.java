0c8c42b9a3bb14a656258b20240fd6cff65be49b
Xhall1 <gridoracones@gmail.com>
2025-09-04 01:20:55 -0500

    feat: Edit books

--- book_store/src/main/java/xl/book_store/view/BookForm.java
+++ book_store/src/main/java/xl/book_store/view/BookForm.java
@@ -8,12 +8,17 @@ import xl.book_store.service.ServiceBook;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 @Component
 public class BookForm extends JFrame {
     ServiceBook serviceBook;
     private JPanel panel;
     private JTable bookTable;
+    private JTextField idText;
     private JTextField bookText;
     private JTextField authorText;
     private JTextField priceText;
@@ -29,6 +34,16 @@ public class BookForm extends JFrame {
         startForm();
 
         addButton.addActionListener(e -> addBook());
+        bookTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                loadBook();
+            }
+        });
+        editButton.addActionListener(e -> {
+            editBook();
+        });
     }
 
     private void startForm(){
@@ -65,17 +80,67 @@ public class BookForm extends JFrame {
         clearForm();
     }
 
+    private void loadBook(){
+        var line = bookTable.getSelectedRow();
+        if(line != -1){
+            String idBook =
+                    bookTable.getModel().getValueAt(line, 0).toString();
+            idText.setText(idBook);
+            String bookName =
+                    bookTable.getModel().getValueAt(line, 1).toString();
+            bookText.setText(bookName);
+            String author =
+                    bookTable.getModel().getValueAt(line, 2).toString();
+            authorText.setText(author);
+            String price =
+                    bookTable.getModel().getValueAt(line, 3).toString();
+            priceText.setText(price);
+            String existence =
+                    bookTable.getModel().getValueAt(line, 4).toString();
+            existenceText.setText(existence);
+        }
+    }
+
+    private void editBook(){
+        if(this.idText.getText().equals("")){
+            showMessage("You must select a record");
+        } else {
+            if(bookText.getText().equals("")){
+                showMessage("Give a name to the book");
+                bookText.requestFocusInWindow();
+                return;
+            }
+
+            int idBook = Integer.parseInt(idText.getText());
+            var bookName = bookText.getText();
+            var author = authorText.getText();
+            var price = Double.parseDouble(priceText.getText());
+            var existence = Integer.parseInt(existenceText.getText());
+            var book =
+                    new Book(idBook, bookName, author, price, existence);
+            serviceBook.saveBook(book);
+            showMessage("The book was modified");
+            clearForm();
+            listBooks();
+        }
+    }
+
     private void clearForm() {
         bookText.setText("");
         authorText.setText("");
         priceText.setText("");
         existenceText.setText("");
     }
+
     private void showMessage(String message) {
         JOptionPane.showMessageDialog(this, message);
     }
 
     private void createUIComponents() {
+
+        idText = new JTextField("");
+        idText.setVisible(false);
+
         this.bookTableModel = new DefaultTableModel(0, 5);
         String[] headers = {"id", "Book", "Author", "Price", "Existence"};
         this.bookTableModel.setColumnIdentifiers(headers);
